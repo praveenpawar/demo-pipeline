137210657bddd1a8dcd1e6018a7e5b8c4eb9f9d4
praveen <praveenpawar414@gmail.com>
2023-03-25 17:55:04 +0530

    file added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name="cencus_income",
+      version="0.0.1",
+      author="praveen",
+      author_email="pvp.pawar@gmail.com",
+      packages=find_packages(),
+      install_requires=["pandas", "numpy", "flask"]
+      )
+
+
+
